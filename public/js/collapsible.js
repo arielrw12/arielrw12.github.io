39c2c63f2bdeb95596af4a37ca29b905a6ecb619
Ariel Webb <arielwebb12@gmail.com>
2019-03-17 18:51:35 -0400

    added other collapsible sections

--- public/js/collapsible.js
+++ public/js/collapsible.js
@@ -1,20 +1,62 @@
 document.getElementsByClassName
 
-collapsible();
+collapsibleOne();
+collapsibleTwo();
+collapsibleThree();
+collapsibleFour();
 
-function collapsible() {
-    bindProjectButtonToCollapsible();
+function collapsibleOne() {
+    bindProjectButtonToCollapsibleOne();
+}
+
+function collapsibleTwo() {
+    bindProjectButtonToCollapsibleTwo();
+}
+
+function collapsibleThree() {
+    bindProjectButtonToCollapsibleThree();
+}
+
+function collapsibleFour() {
+    bindProjectButtonToCollapsibleFour();
 }
 
 function toggleHide(element) {
     element.classList.toggle('hide');
 }
 
-function bindProjectButtonToCollapsible() {
-    const projectButton = document.querySelector('.project__button');
+function bindProjectButtonToCollapsibleOne() {
+    const projectButton = document.querySelector('.project__button-1');
 
     projectButton.addEventListener('click', function() {
-        const collapsible = document.querySelector('.project__collapsible');
+        const collapsible = document.querySelector('.project__collapsible-1');
+        toggleHide(collapsible);
+    });
+}
+
+function bindProjectButtonToCollapsibleTwo() {
+    const projectButton = document.querySelector('.project__button-2');
+
+    projectButton.addEventListener('click', function() {
+        const collapsible = document.querySelector('.project__collapsible-2');
+        toggleHide(collapsible);
+    });
+}
+
+function bindProjectButtonToCollapsibleThree() {
+    const projectButton = document.querySelector('.project__button-3');
+
+    projectButton.addEventListener('click', function() {
+        const collapsible = document.querySelector('.project__collapsible-3');
+        toggleHide(collapsible);
+    });
+}
+
+function bindProjectButtonToCollapsibleFour() {
+    const projectButton = document.querySelector('.project__button-4');
+
+    projectButton.addEventListener('click', function() {
+        const collapsible = document.querySelector('.project__collapsible-4');
         toggleHide(collapsible);
     });
 }
